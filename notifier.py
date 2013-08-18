cff1362e97cbea691b00b5c3c33b3760672b0418
shalinshah1993 <shalinshah1993@gmail.com>
2013-08-18 12:04:00 +0530

    Added 3rd option for FB/GMAIL notification

--- notifier.py
+++ notifier.py
@@ -38,11 +38,11 @@ class Gmail:
 			return 0
 		return int(matches[0])
 
-	def getfeed(self):
+	def getfeed(self,auth):
 		print 'Checking...'
 		conn = httplib.HTTPSConnection(serv)
 		conn.putrequest('GET', path)
-		conn.putheader('Authorization', 'Basic %s'%self.auth)
+		conn.putheader('Authorization', 'Basic %s'%auth)
 		conn.endheaders()
 		return conn.getresponse().read()
 
@@ -53,12 +53,12 @@ class Gmail:
 			if time.time() - last_check < INTERVAL*60:
 				continue
 			last_check = time.time()
-			msgs = self.count(self.getfeed())
+			msgs = self.count(self.getfeed(self.auth))
 			print msgs,'mails'
 			writeSer(GMAIL,str(msgs))
 
 
-class facebook():
+class Facebook:
 
 	def __init__(self):
 		self.counter = 0
@@ -95,17 +95,69 @@ class facebook():
 		print "Total Notifications :", self.counter
 
 	def __del__(self):
-		print "OKAY"
-		fb.logout()
+		fb.logout() 
 
+class fb_gmail():
+	def __init__(self):
+		print "Enter your GMAIL details:"
+		self.auth = base64.encodestring('%s:%s'%(raw_input('Username: '),getpass.getpass()))
+		print "Authenticating your FB details:"
+		fb.AUTH_SCOPE = ['read_stream','manage_notifications','read_mailbox','read_requests','user_status']
+		fb.authenticate()
+		
+		last_check1 = time.time() - INTERVAL * 60 # GMAIL check
+		last_check2 = time.time() - INTERVAL * 60 + 30 # FB check
+		fCounter = 0
+		gCounter = 0
+		
+		while True:
+			if time.time() - last_check1 > 60 * INTERVAL:
+				last_check1 = time.time()
+				gCounter = self.count(self.getfeed(self.auth))
+				print "Unread Mails on GMAIL :", gCounter
+				writeSer(GMAIL,str(gCounter))
+				
+			if time.time() - last_check2 > 60 * INTERVAL:
+				last_check2 = time.time()
+				userId = fb.fql("SELECT uid,name FROM user WHERE uid = me()")
+				notify = fb.fql("SELECT notification_id,created_time,title_text FROM notification WHERE recipient_id = me() AND is_unread = 1")
+				mail = fb.fql("SELECT name,unread_count FROM mailbox_folder WHERE folder_id = 0 AND viewer_id = me()")
+				
+				if len(mail):
+					fCounter += 1
+				if len(notify):
+					fCounter += len(notify)
+				writeSer(FB,fCounter)
+				print "Notifiactions on FB :",fCounter
+				fCounter = 0
+				
+	def getfeed(self,auth):
+		conn = httplib.HTTPSConnection(serv)
+		conn.putrequest('GET', path)
+		conn.putheader('Authorization', 'Basic %s'%auth)
+		conn.endheaders()
+		return conn.getresponse().read()
+	      
+	def count(self,data):
+		matches = re.findall('<fullcount>([0-9]+)</fullcount>', data)
+		if len(matches) == 0:
+			print 'Error in parsing feed, check user name and password are correct'
+			return 0
+		return int(matches[0])
+	      
+	def __del__(init):
+		fb.logout()
 			
 if __name__ == '__main__':
-	choice = input("Welome to Command line FB-Gmail Notifier:-\n-Please type '1' for GMAIL\n-Please type '2' for FB\nYour input : ")
+	choice = input("Welome to Command line FB-Gmail Notifier:-\n-Please type '1' for GMAIL\n-Please type '2' for FB\n-Please type '3' for GMAIL and FB both\nYour input : ")
 	if choice == 1:
 		print "Started GMAIL Notification Service...."
 		gmail = Gmail()
 	elif choice == 2:
 		print "Started FB Notification Service....."
-		fb = facebook()
+		fb = Facebook()
+	elif choice == 3:
+		print "Started FB and GMAIL notification Service....."
+		fb_gmail()
 	else:
 		print "Please enter a valid choice."
